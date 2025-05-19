9ee6bb7f388d22b35567900ac4b80133f16b5d4f
與古田和叶 <oriika0305@oriika-mac-air.local>
2025-05-19 09:50:01 +0900

    ローディングアニメーションの追加

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // JavaScriptでの動きが必要ならここに追加
-console.log("ようこそ YOKODA AIKA PORTFOLIO へ！");
+<script>
+  window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+    loader.style.opacity = "0";
+    loader.style.pointerEvents = "none";
+    setTimeout(() => loader.style.display = "none", 500);
+  });
+</script>
