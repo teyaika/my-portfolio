05d859769a4ed3a01ee35c9cd638502b539a44a6
與古田和叶 <oriika0305@oriika-mac-air.local>
2025-05-19 09:53:32 +0900

    ローディングアニメーションの追加

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // JavaScriptでの動きが必要ならここに追加
-<script>
-  window.addEventListener("load", function () {
+window.addEventListener("load", function () {
     const loader = document.getElementById("loader");
     loader.style.opacity = "0";
     loader.style.pointerEvents = "none";
     setTimeout(() => loader.style.display = "none", 500);
-  });
-</script>
+});
